d6948fb80c4dacfb7c57d9a995210e7c3bc4586d
LucasJenkins731 <lgjenkins@quinnipiac.edu>
2025-02-19 20:00:44 -0500

    Continuously asks if wrong input in entered

--- animalpicker.java
+++ animalpicker.java
@@ -4,21 +4,31 @@ import java.util.*;
 
 public class animalpicker {
     public static String selection;
-    public static void main(String[] args) {
+    public static int tracker = 0;
+
+    static void input() {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("What animal would you like to see? \nCat or Dog?");
-
         selection = scanner.next().toLowerCase();
+    }
+    public static void main(String[] args) {
 
-        if (selection.equals("cat")) {
-            System.out.println("CAT ASCII ART HERE");
-        }
-        else if (selection.equals("dog")) {
-            System.out.println("DOG ASCII ART HERE");
-        }
-        else {
-            System.out.println("Invalid input");
+        animalpicker.input();
+
+        while (tracker == 0){
+            if (selection.equals("cat")) {
+                tracker = 1;
+                System.out.println("CAT ASCII ART HERE");
+            }
+            else if (selection.equals("dog")) {
+                tracker = 1;
+                System.out.println("DOG ASCII ART HERE");
+            }
+            else {
+                tracker = 0;
+                System.out.println("Invalid input");
+                animalpicker.input();
+            }
         }
     }
 }
